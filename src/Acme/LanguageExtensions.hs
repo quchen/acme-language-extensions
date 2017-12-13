674a18ef5016880ad0da3ef7ff77ad38d9dd35ec
David Luposchainsky <dluposchainsky@gmail.com>
2017-12-13 14:58:27 +0100

    Add doctest

--- src/Acme/LanguageExtensions.hs
+++ src/Acme/LanguageExtensions.hs
@@ -1,7 +1,18 @@
-{-# OPTIONS_GHC -w #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 
 module Acme.LanguageExtensions where
 
+-- | Are BangPatterns enabled?
+--
+-- >>> :set -XNoBangPatterns
+-- >>> let f _ = False in let f !y = y in f True
+-- False
+--
+-- >>> :set -XBangPatterns
+-- >>> let f _ = False in let f !y = y in f True
+-- True
 bangPatterns :: Bool
 bangPatterns = let f _ = False
                in let f !y = y
