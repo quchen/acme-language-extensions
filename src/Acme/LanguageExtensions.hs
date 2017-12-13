37905ef496752248fcbac8f0be7b17fb87e065dd
David Luposchainsky <dluposchainsky@gmail.com>
2017-12-13 19:19:16 +0100

    MagicHash, negative literals, CPP

--- src/Acme/LanguageExtensions.hs
+++ src/Acme/LanguageExtensions.hs
@@ -1,19 +1,36 @@
+{-# OPTIONS_GHC -Wno-missing-methods #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -Wno-unused-local-binds #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -Woverlapping-patterns #-}
+{-# OPTIONS_GHC -Woverlapping-patterns #-}
 
 module Acme.LanguageExtensions where
-
-import Data.Functor.Identity
 
 bangPatterns :: Bool
 bangPatterns = let f _ = False
                in let f !y = y
                   in f True
 
-rebindableSyntax :: Bool
-rebindableSyntax = runIdentity (do
-    let _ >> _ = pure True
-    Identity ()
-    Identity False
-    )
+{-
+#define False True
+-}
+cpp :: Bool
+cpp = False
+{-
+#undef False
+-}
+
+negativeLiterals :: Bool
+negativeLiterals = (-1 :: NegativeLiterals) == 0
+newtype NegativeLiterals = NegativeLiterals Int deriving (Eq, Ord)
+instance Num NegativeLiterals where
+    negate _ = NegativeLiterals 0
+    fromInteger _ = NegativeLiterals 1
+
+magicHash :: Bool
+magicHash = foo# ()
+  where
+    _ # _ = False
+    foo# _ = True
+    foo = ()
